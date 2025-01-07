f7e0d73d542f288a22d1f194979f43c3b55d8b8c
Andy Walter <musictopia2@hotmail.com>
2025-01-07 15:21:46 -0700

    Add project files.

---
+++ ILengthCache.cs
@@ -0,0 +1,6 @@
+﻿namespace MediaHelpers.MediaLengthCoreLibrary;
+public interface ILengthCache
+{
+    int? GetLength(string path); //null means not found.
+    DateOnly EarliestDateToUse { get; } //so anything before can attempt this list.
+}
---
+++ LengthClass.cs
@@ -0,0 +1,46 @@
+﻿namespace MediaHelpers.MediaLengthCoreLibrary;
+public static class LengthClass
+{
+    public static ILengthCache? LengthCache { get; set; }
+    public static int Length(string tempPath)
+    {
+        // Check if the file exists
+        if (File.Exists(tempPath))
+        {
+            FileInfo temp = new (tempPath);
+            DateTime firsts = temp.LastWriteTime;
+            DateOnly lastWriteDate = DateOnly.FromDateTime(firsts);
+            try
+            {
+                if (LengthCache is not null)
+                {
+                    DateOnly earliestCacheDate = LengthCache.EarliestDateToUse;
+                    if (lastWriteDate < earliestCacheDate)
+                    {
+                        int? value;
+                        value = LengthCache.GetLength(tempPath);
+                        if (value is not null)
+                        {
+                            return value.Value;
+                        }
+                    }                       
+                }
+                // Load the media file using TagLibSharp
+                var file = TagLib.File.Create(tempPath);
+
+                // Get the duration of the media file
+                var duration = file.Properties.Duration;
+
+                // Return the length in seconds
+                return (int)duration.TotalSeconds;
+            }
+            catch (Exception ex)
+            {
+                // Handle any errors, such as unsupported file formats
+                Console.WriteLine($"Error reading media file: {ex.Message}");
+            }
+        }
+        // Return -1 if file does not exist or error occurs
+        return -1;
+    }
+}
